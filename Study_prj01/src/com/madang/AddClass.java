85d8f793b4b0e9f3a387a234294ffa9a9336ccd1
mc709 <mc709@hanmail.net>
2023-05-04 14:25:34 +0900

    과제 연동 적용

---
+++ Study_prj01/src/com/madang/AddClass.java
@@ -0,0 +1,5 @@
+package com.madang;
+
+public class AddClass {
+
+}
--- Study_prj01/src/com/madang/MadangMain.java
+++ Study_prj01/src/com/madang/MadangMain.java
@@ -4,6 +4,9 @@ public class MadangMain {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Java~"); 
+		
+		//변경
+		System.out.println("Git연동 변경 적용");
 	}
 
 }
